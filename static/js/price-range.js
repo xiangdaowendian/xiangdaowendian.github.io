62f5f052b9f053138ba9b1afbf7d5a45143b69cc
xiangdaowendian <676432241@QQ.com>
2021-11-21 21:48:42 +0800

    o

--- static/js/price-range.js
+++
@@ -1,1 +0,0 @@
-$((function(){$("#slider-range").slider({range:!0,min:0,max:500,values:[75,300],slide:function(e,a){$("#amount").val("$"+a.values[0]+" - $"+a.values[1])}}),$("#amount").val("$"+$("#slider-range").slider("values",0)+" - $"+$("#slider-range").slider("values",1))}));
